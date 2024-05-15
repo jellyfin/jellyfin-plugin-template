57e135ffb9a7a19f1a07c6f8a8ef2ea5d0883814
Nathan McCrina <nathan.mccrina@outlook.com>
2024-05-15 14:11:33 -0500

    update sample project to .net 8

--- Jellyfin.Plugin.Template/Configuration/PluginConfiguration.cs
+++ Jellyfin.Plugin.Template/Configuration/PluginConfiguration.cs
@@ -1,57 +1,58 @@
 using MediaBrowser.Model.Plugins;
 
-namespace Jellyfin.Plugin.Template.Configuration;
-
-/// <summary>
-/// The configuration options.
-/// </summary>
-public enum SomeOptions
+namespace Jellyfin.Plugin.Template.Configuration
 {
     /// <summary>
-    /// Option one.
+    /// The configuration options.
     /// </summary>
-    OneOption,
-
-    /// <summary>
-    /// Second option.
-    /// </summary>
-    AnotherOption
-}
-
-/// <summary>
-/// Plugin configuration.
-/// </summary>
-public class PluginConfiguration : BasePluginConfiguration
-{
-    /// <summary>
-    /// Initializes a new instance of the <see cref="PluginConfiguration"/> class.
-    /// </summary>
-    public PluginConfiguration()
+    public enum SomeOptions
     {
-        // set default options here
-        Options = SomeOptions.AnotherOption;
-        TrueFalseSetting = true;
-        AnInteger = 2;
-        AString = "string";
+        /// <summary>
+        /// Option one.
+        /// </summary>
+        OneOption,
+
+        /// <summary>
+        /// Second option.
+        /// </summary>
+        AnotherOption
     }
 
     /// <summary>
-    /// Gets or sets a value indicating whether some true or false setting is enabled..
+    /// Plugin configuration.
     /// </summary>
-    public bool TrueFalseSetting { get; set; }
+    public class PluginConfiguration : BasePluginConfiguration
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PluginConfiguration"/> class.
+        /// </summary>
+        public PluginConfiguration()
+        {
+            // set default options here
+            Options = SomeOptions.AnotherOption;
+            TrueFalseSetting = true;
+            AnInteger = 2;
+            AString = "string";
+        }
 
-    /// <summary>
-    /// Gets or sets an integer setting.
-    /// </summary>
-    public int AnInteger { get; set; }
+        /// <summary>
+        /// Gets or sets a value indicating whether some true or false setting is enabled..
+        /// </summary>
+        public bool TrueFalseSetting { get; set; }
 
-    /// <summary>
-    /// Gets or sets a string setting.
-    /// </summary>
-    public string AString { get; set; }
+        /// <summary>
+        /// Gets or sets an integer setting.
+        /// </summary>
+        public int AnInteger { get; set; }
 
-    /// <summary>
-    /// Gets or sets an enum option.
-    /// </summary>
-    public SomeOptions Options { get; set; }
+        /// <summary>
+        /// Gets or sets a string setting.
+        /// </summary>
+        public string AString { get; set; }
+
+        /// <summary>
+        /// Gets or sets an enum option.
+        /// </summary>
+        public SomeOptions Options { get; set; }
+    }
 }
